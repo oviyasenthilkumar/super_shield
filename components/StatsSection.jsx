99c709800c6914043f1a8a588eb52897f7c175a2
oviyasenthilkumar <154113657+oviyasenthilkumar@users.noreply.github.com>
2025-05-07 11:47:29 +0530

    Update StatsSection.jsx

--- components/StatsSection.jsx
+++ components/StatsSection.jsx
@@ -11,7 +11,7 @@ const StatsSection = () => {
 
   return (
     <section className="py-10 px-4 bg-white">
-      <div className="max-w-8xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+      <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
         {stats.map((stat, index) => (
           <div
             key={index}
